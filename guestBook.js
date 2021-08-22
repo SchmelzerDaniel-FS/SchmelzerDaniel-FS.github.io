86d1a4dfcd513943ff2d84c72d98a11afba1e071
SchmelzerDaniel-FS <70539787+SchmelzerDaniel-FS@users.noreply.github.com>
2021-08-22 15:29:27 -0500

    Modal for JS application
    Set up a simple Modal popup window for the Guest Book.

---
+++ guestBook.js
@@ -0,0 +1,36 @@
+var x = 0;
+var array = Array();
+
+function add_element_to_array()
+{
+ array[x] = document.getElementById("text1").value;
+ alert("Thank you " + array[x] + ". You have been added at index " + x);
+ x++;
+ document.getElementById("text1").value = "";
+}
+
+function display_array()
+{
+   var e = "<hr/>";   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     e += "Element " + y + " = " + array[y] + "<br/>";
+   }
+   document.getElementById("Result").innerHTML = e;
+}
+
+var modal = document.getElementById("bookModal");
+var btn = document.getElementById("openBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
